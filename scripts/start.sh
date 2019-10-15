593e2391cc0cc6d4b59f73649b0bab925cbf3959
GSansigolo <gabrielsansigolo@gmail.com>
2019-10-15 14:48:24 -0300

    Fix apiVersion

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
-kubectl create -f kubernetes/local-volumes.yaml --validate=false
-kubectl create -f kubernetes/postgres.yaml --validate=false
-kubectl create -f kubernetes/drupal.yaml --validate=false
+kubectl create -f kubernetes/local-volumes.yaml
+kubectl create -f kubernetes/postgres.yaml
+kubectl create -f kubernetes/drupal.yaml
 kubectl get nodes
 kubectl get svc drupal
