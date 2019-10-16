c6da08d6b4a57fef991eeaf688560f9031f3026d
GSansigolo <gabrielsansigolo@gmail.com>
2019-10-16 12:21:17 -0300

    Add scripts

---
+++ scripts/start.sh
@@ -0,0 +1,5 @@
+kubectl create -f kubernetes/local-volumes.yaml
+kubectl create -f kubernetes/postgres.yaml
+kubectl create -f kubernetes/drupal.yaml
+kubectl get nodes
+kubectl get svc drupal
--- src/main/java/com/example/postgresdemo/ValueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.postgresdemo;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ValueRepository extends CrudRepository<Value, Long>{
-    List<Value> findByValue(String value);
-}
