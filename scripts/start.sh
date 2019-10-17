4ca26082d467f82a7d6266bc728b6b4c0c93b22f
GSansigolo <gabrielsansigolo@gmail.com>
2019-10-17 16:47:17 -0300

    Add nginx

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,11 @@
+#nginx
+kubectl create -f kubernetes/nginx.yaml
+
 #postgres
 kubectl create -f kubernetes/postgres-configmap.yaml 
 kubectl create -f kubernetes/postgres-storage.yaml 
 kubectl create -f kubernetes/postgres-deployment.yaml 
 kubectl create -f kubernetes/postgres-service.yaml
-
-kubectl apply -f kubernetes/nginx.yaml
 
 kubectl get nodes
 kubectl get svc postgres
