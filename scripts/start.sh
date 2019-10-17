4afdef6edcb88c330e9eb1403b0081d92653f683
GSansigolo <gabrielsansigolo@gmail.com>
2019-10-17 16:09:26 -0300

    Add nginx

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,8 @@ kubectl create -f kubernetes/postgres-storage.yaml
 kubectl create -f kubernetes/postgres-deployment.yaml 
 kubectl create -f kubernetes/postgres-service.yaml
 
+kubectl apply -f kubernetes/nginx.yaml
+
 kubectl get nodes
 kubectl get svc postgres
 
-kubectl expose deployment postgres --type=LoadBalancer --port=32130 --target-port=5432
