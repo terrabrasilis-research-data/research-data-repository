daf7632be3b623b9fd18543dacae7931bafff17f
GSansigolo <gabrielsansigolo@gmail.com>
2019-10-18 17:04:37 -0300

    Fix LoadBalance

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,8 @@ kubectl create -f kubernetes/nginx.yaml
 kubectl create -f kubernetes/postgres-configmap.yaml 
 kubectl create -f kubernetes/postgres-storage.yaml 
 kubectl create -f kubernetes/postgres-deployment.yaml 
-kubectl create -f kubernetes/postgres-service.yaml
+#kubectl create -f kubernetes/postgres-service.yaml
+kubectl expose deployment postgres --type=LoadBalancer --name=postgres
 
 kubectl get nodes
 kubectl get svc postgres
