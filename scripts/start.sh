53afdd103441d5eda57c63742d1a4c42b8080f19
GSansigolo <gabrielsansigolo@gmail.com>
2019-11-26 17:24:04 -0300

    WIP OwnCloud

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,18 @@
-#namespace
-kubectl create -f kubernetes/namespace.json
-
 #PersistentVolume
 kubectl apply -f kubernetes/pv.yaml
 kubectl apply -f kubernetes/pv-claim.yaml
 
 #postgis
-kubectl apply -f kubernetes/postgres-deployment.yaml
-kubectl apply -f kubernetes/postgres-service.yaml
+kubectl apply -f kubernetes/postgis-deployment.yaml
+kubectl apply -f kubernetes/postgis-service.yaml
 
 #geoserver
 kubectl apply -f kubernetes/geoserver-deployment.yaml
 kubectl apply -f kubernetes/geoserver-service.yaml
+
+#owncloud
+kubectl apply -f kubernetes/owncloud-deployment.yaml
+kubectl apply -f kubernetes/owncloud-service.yaml
 
 #geonetwork
 #kubectl apply -f kubernetes/geonetwork-deployment.yaml
